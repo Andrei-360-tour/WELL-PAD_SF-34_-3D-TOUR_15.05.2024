d6f5a791f5a0a4c29afacb449bf90c8ee2a4ce74
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-06-04 09:11:39 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,719 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-34_wellhead",
+      "name": "SF-34_WellHead",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.13872328218988095,
+        "pitch": 0.28895355580351634,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0709427050496103,
+          "pitch": 0.3959430962462456,
+          "rotation": 0,
+          "target": "1-sf-34_wellheadcellar"
+        },
+        {
+          "yaw": -1.8229112660606503,
+          "pitch": 0.521381387787418,
+          "rotation": 0,
+          "target": "2-sf-34_wellhead--concrete-pad"
+        },
+        {
+          "yaw": 0.8861465652651397,
+          "pitch": -0.012769103978639507,
+          "rotation": 0,
+          "target": "11-sf-34_sucker-rod-pupping-system"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-34_wellheadcellar",
+      "name": "SF-34_WellHead&Cellar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.02866211387944162,
+        "pitch": 0.6656316186344533,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9401534738313266,
+          "pitch": 0.6298142010967869,
+          "rotation": 0,
+          "target": "0-sf-34_wellhead"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-34_wellhead--concrete-pad",
+      "name": "SF-34_WellHead & Concrete Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.011802046891542517,
+        "pitch": 0.2986660132307595,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.36088886342679416,
+          "pitch": 0.7804264614058898,
+          "rotation": 0,
+          "target": "1-sf-34_wellheadcellar"
+        },
+        {
+          "yaw": -2.7025370829061472,
+          "pitch": 0.38383251096001025,
+          "rotation": 0,
+          "target": "3-sf-34_wellhead--pipeline"
+        },
+        {
+          "yaw": 0.9309992572777865,
+          "pitch": 0.03187558716405903,
+          "rotation": 0,
+          "target": "10-sf-34_security-building"
+        },
+        {
+          "yaw": -1.4704063772714875,
+          "pitch": 0.15045360298774568,
+          "rotation": 0,
+          "target": "13-sf-34_over-head-line-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-34_wellhead--pipeline",
+      "name": "SF-34_WellHead & Pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.138523072887118,
+        "pitch": -0.18917217127918917,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.02014638392008,
+          "pitch": 0.4391246418459893,
+          "rotation": 0,
+          "target": "2-sf-34_wellhead--concrete-pad"
+        },
+        {
+          "yaw": 0.2340252425995768,
+          "pitch": 0.2110518656264322,
+          "rotation": 0,
+          "target": "4-sf-34_pipeline-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-34_pipeline-1",
+      "name": "SF-34_Pipeline 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.3108917960957367,
+        "pitch": 0.12110400063907534,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.968397473248233,
+          "pitch": 0.29798996700653113,
+          "rotation": 0,
+          "target": "3-sf-34_wellhead--pipeline"
+        },
+        {
+          "yaw": 0.06403786324067084,
+          "pitch": 0.1389831216156754,
+          "rotation": 0,
+          "target": "5-sf-34_pipeline-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-34_pipeline-2",
+      "name": "SF-34_Pipeline 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7869722727849418,
+          "pitch": 0.20117726477423048,
+          "rotation": 0,
+          "target": "4-sf-34_pipeline-1"
+        },
+        {
+          "yaw": 2.05568039385341,
+          "pitch": -0.005988985570844818,
+          "rotation": 0,
+          "target": "13-sf-34_over-head-line-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-34_over-head-line-1",
+      "name": "SF-34_Over Head Line 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.794315039023587,
+        "pitch": -0.10751721808297887,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1312461802880733,
+          "pitch": 0.01247674212629768,
+          "rotation": 0,
+          "target": "13-sf-34_over-head-line-1"
+        },
+        {
+          "yaw": 1.6063577700289313,
+          "pitch": 0.0398895907112049,
+          "rotation": 0,
+          "target": "7-sf-34_well-pad-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sf-34_well-pad-1",
+      "name": "SF-34_Well Pad 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.164949842628589,
+        "pitch": -0.04750120873420727,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.607097156178341,
+          "pitch": 0.13136951224148952,
+          "rotation": 0,
+          "target": "6-sf-34_over-head-line-1"
+        },
+        {
+          "yaw": 1.8229224861977649,
+          "pitch": 0.032591096579828616,
+          "rotation": 0,
+          "target": "8-sf-34_well-pad-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-sf-34_well-pad-2",
+      "name": "SF-34_Well Pad 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.7879957429748794,
+        "pitch": 0.25942110836864885,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.900454081499735,
+          "pitch": 0.1477436432566659,
+          "rotation": 0,
+          "target": "7-sf-34_well-pad-1"
+        },
+        {
+          "yaw": 1.1604232687160518,
+          "pitch": 0.005760128306205203,
+          "rotation": 0,
+          "target": "9-sf-34_pipeline-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sf-34_pipeline-3",
+      "name": "SF-34_Pipeline 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.3964236272185495,
+        "pitch": 0.12224640735096592,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4104207139518099,
+          "pitch": 0.09156692480694417,
+          "rotation": 0,
+          "target": "11-sf-34_sucker-rod-pupping-system"
+        },
+        {
+          "yaw": 2.6460002733388857,
+          "pitch": 0.2097797305601894,
+          "rotation": 0,
+          "target": "8-sf-34_well-pad-2"
+        },
+        {
+          "yaw": -0.29479042234400055,
+          "pitch": 0.05097205838689334,
+          "rotation": 0,
+          "target": "10-sf-34_security-building"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-sf-34_security-building",
+      "name": "SF-34_Security building",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.1988099749626038,
+        "pitch": -0.07169114214313765,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8609487510486815,
+          "pitch": 0.12360340304970663,
+          "rotation": 0,
+          "target": "9-sf-34_pipeline-3"
+        },
+        {
+          "yaw": 1.6007518554828524,
+          "pitch": 0.09471584642065878,
+          "rotation": 0,
+          "target": "11-sf-34_sucker-rod-pupping-system"
+        },
+        {
+          "yaw": 1.3233082123387234,
+          "pitch": 0.004822135008303974,
+          "rotation": 0,
+          "target": "2-sf-34_wellhead--concrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-sf-34_sucker-rod-pupping-system",
+      "name": "SF-34_Sucker rod pupping system",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.6615054010102668,
+        "pitch": 0.03264994688542444,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.087317890116692,
+          "pitch": 0.14870008993928252,
+          "rotation": 0,
+          "target": "9-sf-34_pipeline-3"
+        },
+        {
+          "yaw": 1.2102119569448018,
+          "pitch": 0.059957522452041445,
+          "rotation": 0,
+          "target": "12-sf-34_concrete-pad"
+        },
+        {
+          "yaw": 0.21128385430145435,
+          "pitch": 0.06079113769334299,
+          "rotation": 0,
+          "target": "0-sf-34_wellhead"
+        },
+        {
+          "yaw": -1.8771017614343908,
+          "pitch": 0.23397244618514357,
+          "rotation": 0,
+          "target": "10-sf-34_security-building"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-sf-34_concrete-pad",
+      "name": "SF-34_Concrete Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.3346739774024261,
+        "pitch": 0.13544156413953168,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.061801117879721,
+          "pitch": 0.14039380936670298,
+          "rotation": 0,
+          "target": "11-sf-34_sucker-rod-pupping-system"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-sf-34_over-head-line-1",
+      "name": "SF-34_Over Head Line 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.7601162346553529,
+        "pitch": -0.3515475381738149,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.275983467139506,
+          "pitch": 0.1454632617593674,
+          "rotation": 0,
+          "target": "5-sf-34_pipeline-2"
+        },
+        {
+          "yaw": 0.24614744830881286,
+          "pitch": 0.0456352917918732,
+          "rotation": 0,
+          "target": "6-sf-34_over-head-line-1"
+        },
+        {
+          "yaw": 2.5381389545635127,
+          "pitch": 0.07922694419026399,
+          "rotation": 0,
+          "target": "2-sf-34_wellhead--concrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD SF-34",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
